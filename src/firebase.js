7a31dd43c0f752cff8df077f9f13007ca368b73e
Mrudul Patel <mrudulpatel04@gmail.com>
2023-04-02 11:55:43 +0000

    frontend build completed

---
+++ src/firebase.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4Q2lcuvVst0_9CR4CexoEwKGbBgRrLn0",
+  authDomain: "netflix-clone-5c664.firebaseapp.com",
+  projectId: "netflix-clone-5c664",
+  storageBucket: "netflix-clone-5c664.appspot.com",
+  messagingSenderId: "39602269193",
+  appId: "1:39602269193:web:a4a7f4434af30a95095cd8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4Q2lcuvVst0_9CR4CexoEwKGbBgRrLn0",
+  authDomain: "netflix-clone-5c664.firebaseapp.com",
+  projectId: "netflix-clone-5c664",
+  storageBucket: "netflix-clone-5c664.appspot.com",
+  messagingSenderId: "39602269193",
+  appId: "1:39602269193:web:a4a7f4434af30a95095cd8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
